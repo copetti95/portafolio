0f9a74b2696e45b659ee7346ca576a0d0b6b10e6
ocopetti <88229533+ocopetti@users.noreply.github.com>
2022-06-29 11:28:10 -0300

    Header Responsive
    29/06/2022
    
    En este commit algo que el header sea responsive, construyéndolo con mobile first.
    
    También elimino varios componentes que incluía en el header y los pongo directamente en uno solo.
    
    Por último, reemplacé el banner por una imagen, y también lo volví responsive.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { LogoApComponent } from './components/logo-ap/logo-ap.component';
-import { LogointiComponent } from './components/logointi/logointi.component';
-import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
-import { ButtonComponent } from './components/button/button.component';
-import { BannerComponent } from './components/banner/banner.component';
 import { PersonalCardComponent } from './components/personal-card/personal-card.component';
 import { ProfilePictureComponent } from './components/profile-picture/profile-picture.component';
 import { ExperienceComponent } from './components/experience/experience.component';
@@ -21,11 +16,6 @@ import { SkillsComponent } from './components/skills/skills.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoApComponent,
-    LogointiComponent,
-    RedesSocialesComponent,
-    ButtonComponent,
-    BannerComponent,
     PersonalCardComponent,
     ProfilePictureComponent,
     ExperienceComponent,
